c11017c494f0aac493a72d6039bdd065bbdf752e
NguyenHongKhanh2311 <hongkhanh2311@gmail.com>
2025-09-19 19:52:39 +0700

    test

---
+++ HW1/Introduction.java
@@ -0,0 +1,19 @@
+public class Introduction {
+    public static void main(String[] args) {
+        String name = "Nguyễn Hồng Khánh";
+        String id = "24021529";
+        String className = "K69-I-CS5";
+        String usernameGitHub = "khanhNguyenHong2311";
+        String email = "24021529@vnu.edu.vn";
+
+        System.out.println(name + "\t" + id + "\t" + className + "\t" + usernameGitHub + "\t" + email);
+
+        for (int i = 9; i >= 2; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println(1 + " bottle of beer on the wall, " + 1 + " bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall");
+    }
+}
